8dd5c5c4880bbe0dd4677de6baf2ab076bf92547
Damian Wolny <damianwolny@gmail.com>
2021-09-25 13:06:21 +0200

    ADD: style for full screen

--- test-task/src/app/map/map.component.ts
+++ test-task/src/app/map/map.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { circle, latLng, marker, polygon, tileLayer } from 'leaflet';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
+import { circle, icon, latLng, Layer, marker, polygon, tileLayer } from 'leaflet';
 import { AppService } from '../services/app.service';
 
 @Component({
@@ -8,6 +8,7 @@ import { AppService } from '../services/app.service';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
+  public markers: Layer[] = [];
   options = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
@@ -15,12 +16,33 @@ export class MapComponent implements AfterViewInit {
     zoom: 5,
     center: latLng(51.8276976, 16.9560484)
   };
-  layers = [
-    marker([ 51.8276976, 16.9560484 ])
-  ];
   constructor(public appService: AppService) { }
 
   ngAfterViewInit(): void {
     this.appService.pageName = 'map';
+    this.addLayer();
+  }
+  addLayer() {
+    const layer = marker([51.8276976, 16.9560484], {
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/plugins/leaflet/images/marker-icon.png',
+        shadowUrl: 'assets/img_static/map/markerNoShadow.png'
+      })
+    });
+    this.markers.push(layer);
+    this.markers[this.markers.length - 1].bindPopup('<a href="#" data-id="id" class="get-details">Test</a>');
+  }
+
+  @HostListener('document:click', ['$event']) 
+  clickout(event: MouseEvent | any) 
+  { 
+    if(event.target.classList.contains("get-details"))
+      this.testControl(event.target.getAttribute("data-id")); 
+  }
+  
+  testControl(id: string) {
+    console.log(id);
   }
 }
